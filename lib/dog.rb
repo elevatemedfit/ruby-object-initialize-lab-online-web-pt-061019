0717fb02348b1b2c2dbd971c3ece5a5990a63f35
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-21 20:30:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
-  def initialize(breed="Mutt")
-    @breed = breed
+  def initialize(name)
+    @name = name
   end
-def breed=(breed)
-  @breed = breed
+def name=(name)
+  @name = name
 end
-def breed
-  @breed
+def name
+  @name
 end
 end
